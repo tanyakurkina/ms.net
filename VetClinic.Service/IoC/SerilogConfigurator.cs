e66513d42cec5907dca692533579150cadd6dd78
tanyakurkina <tanyakurkina04@gmail.com>
2024-10-14 23:17:14 +0300

    Update README.md

---
+++ VetClinic.Service/IoC/SerilogConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+
+namespace VetClinic.Service.IoC;
+
+public static class SerilogConfigurator
+{
+    public static void ConfigureService(WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .Enrich.WithCorrelationId()
+                .ReadFrom.Configuration(context.Configuration);
+        });
+
+        builder.Services.AddHttpContextAccessor();
+    }
+
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSerilogRequestLogging();
+    }
+}
